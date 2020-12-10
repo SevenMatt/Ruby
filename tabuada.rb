9e26a75dc3fbc22c55bdd01965472d2647dc5c98
SevenMatt <matheusmamedd@gmail.com>
2020-12-10 12:01:59 -0300

    tabuada

---
+++ tabuada.rb
@@ -0,0 +1,14 @@
+for i in 1..10
+    puts "#{i} * 1 = #{1 * i}"
+    puts "#{i} * 2 = #{2 * i}"
+    puts "#{i} * 3 = #{3 * i}"
+    puts "#{i} * 4 = #{4 * i}"
+    puts "#{i} * 5 = #{5 * i}"
+    puts "#{i} * 6 = #{6 * i}"
+    puts "#{i} * 7 = #{7 * i}"
+    puts "#{i} * 8 = #{8 * i}"
+    puts "#{i} * 9 = #{9 * i}"
+    puts "#{i} * 10 = #{10 * i}"
+    puts
+end
+
